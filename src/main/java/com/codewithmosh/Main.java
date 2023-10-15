c90dd08e0ac618d3c53a5b1673d763a69cb5eedc
Stiven Medina <stvnm33@gmail.com>
2023-10-15 15:02:40 -0500

    Patron Memento

---
+++ src/main/java/com/codewithmosh/Main.java
@@ -0,0 +1,27 @@
+package com.codewithmosh;
+
+import com.codewithmosh.memento.Editor;
+import com.codewithmosh.memento.History;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Editor editor = new Editor();
+        var history = new History();
+        editor.setContent("a");
+        history.push(editor.createState());
+        editor.setContent("b");
+        history.push(editor.createState());
+        editor.setContent("c");
+
+       editor.restore(history.pop());
+        System.out.println(editor.getContent());
+
+        editor.restore(history.pop());
+        System.out.println(editor.getContent());
+
+
+
+    }
+}
---
+++ src/main/java/com/codewithmosh/memento/Editor.java
@@ -0,0 +1,26 @@
+package com.codewithmosh.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Editor {
+
+    private String content;
+
+   public EditorState createState(){
+       return new EditorState(content);
+   }
+
+   public void restore(EditorState state){
+        content = state.getContent();
+   }
+
+   public String getContent(){
+       return content;
+   }
+
+   public void setContent(String content){
+       this.content = content;
+   }
+}
