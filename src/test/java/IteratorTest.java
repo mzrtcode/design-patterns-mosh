353b964969b4fdf9580f0a17be3f401896968822
Stiven Medina <stvnm33@gmail.com>
2023-10-16 17:07:59 -0500

    Patron Strategy Problema

---
+++ src/test/java/IteratorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.codewithmosh.iterator.BrowseHistory;
+
+public class IteratorTest {
+    
+    @Test
+    public void iteratorPrueba(){
+        
+        int valor = 1;
+
+        assertEquals(valor, 1);
+    }
+
+    
+}
