db0329f428e4a6efd9c27ade2443a0e37cd18a3b
Stiven Medina <stvnm33@gmail.com>
2023-10-19 11:28:14 -0500

    Patron Command Implementando Undo() operacion

--- src/main/java/com/codewithmosh/Main.java
+++ src/main/java/com/codewithmosh/Main.java
@@ -4,8 +4,12 @@ import com.codewithmosh.command.BlackAndWhiteCommand;
 import com.codewithmosh.command.Button;
 import com.codewithmosh.command.CompositeCommand;
 import com.codewithmosh.command.ResizeCommand;
+import com.codewithmosh.command.editor.BoldCommand;
+import com.codewithmosh.command.editor.HtmlDocument;
+import com.codewithmosh.command.editor.UndoCommand;
 import com.codewithmosh.command.fx.AddCustomerCommand;
 import com.codewithmosh.command.fx.CustomerService;
+import com.codewithmosh.command.editor.History;
 import com.codewithmosh.strategy.HighContrastFilter;
 import com.codewithmosh.strategy.ImageStore;
 import com.codewithmosh.strategy.JpegCompressor;
@@ -15,11 +19,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-       var composite = new CompositeCommand();
-       composite.add(new ResizeCommand());
-       composite.add(new BlackAndWhiteCommand());
-       composite.execute();
-       composite.execute();
-       composite.execute();
+        var history = new History();
+        var document = new HtmlDocument();
+        document.setContent("Hello World");
+
+        var boldCommand = new BoldCommand(document, history);
+        boldCommand.execute();
+        System.out.println(document.getContent());
+
+        var undoCommand = new UndoCommand(history);
+        undoCommand.execute();
+        System.out.println(document.getContent());
     }
 }
---
+++ src/main/java/com/codewithmosh/command/editor/UndoableCommand.java
@@ -0,0 +1,7 @@
+package com.codewithmosh.command.editor;
+
+public interface UndoableCommand extends Command{
+
+    void unexecute();
+    
+}
